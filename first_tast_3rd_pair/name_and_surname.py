e3832d89a2e824cf91785d737b9c20ddf5014569
KaanErbayraktar <127408213+KaanErbayraktar@users.noreply.github.com>
2023-09-21 16:24:17 +0300

    Delete first_tast_3rd_pair directory

--- first_tast_3rd_pair/name_and_surname.py
+++
@@ -1,9 +0,0 @@
-
-
-list = []
-for i in range(1,11):
-    name = input("{}. ismi giriniz: ".format(i))
-    surname = input("{}. soyismi giriniz: ".format(i))
-    fullname = name + " " + surname
-    list.append(fullname)
-print(list)
